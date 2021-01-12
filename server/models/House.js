5bb032d7957d5c3b29c77cbd26c0a0d849ecb1fc
JordanJStone <statictrack@gmail.com>
2021-01-12 16:12:15 -0700

    finished House model

--- server/models/House.js
+++ server/models/House.js
@@ -5,9 +5,12 @@ const House = new Schema(
   {
     bedrooms: { type: Number, required: true },
     bathrooms: { type: Number, required: true },
-    floors: { type: Number, required: true }
-      levels:
-      price:
-    description
+    floors: { type: Number, required: true },
+    levels: { type: Number, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, maxlength: 50 },
+    imgUrl: { type: String, default: "http://placehold.it/200x200" }
   }
 )
+
+export default House
