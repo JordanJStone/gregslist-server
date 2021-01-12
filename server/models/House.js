25685363114b0845ca4237e2ba041cd40adee8dc
JordanJStone <statictrack@gmail.com>
2021-01-12 16:53:37 -0700

    Houses operational and tested

--- server/models/House.js
+++ server/models/House.js
@@ -6,7 +6,6 @@ const House = new Schema(
     bedrooms: { type: Number, required: true },
     bathrooms: { type: Number, required: true },
     floors: { type: Number, required: true },
-    levels: { type: Number, required: true },
     price: { type: Number, required: true },
     description: { type: String, maxlength: 50 },
     imgUrl: { type: String, default: "http://placehold.it/200x200" }
